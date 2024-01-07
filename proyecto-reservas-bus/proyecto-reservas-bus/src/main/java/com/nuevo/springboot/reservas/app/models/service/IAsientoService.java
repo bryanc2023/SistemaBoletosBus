953136f8e6aa8f447cf74ee621ffea6da3dc284c
BryanCastelo <bryan.castelo@espoch.edu.ec>
2024-01-07 12:11:25 -0500

    Correcion dependencias
    Correncion de dependencias

--- proyecto-reservas-bus/proyecto-reservas-bus/src/main/java/com/nuevo/springboot/reservas/app/models/service/IAsientoService.java
+++ proyecto-reservas-bus/proyecto-reservas-bus/src/main/java/com/nuevo/springboot/reservas/app/models/service/IAsientoService.java
@@ -1,8 +1,15 @@
 package com.nuevo.springboot.reservas.app.models.service;
 
+import java.util.List;
+
 import com.nuevo.springboot.reservas.app.models.entity.Asiento;
 
 
-public interface IAsientoService extends GenericDataService <Asiento> {
+
+public interface IAsientoService {
+	public void save(Asiento entity);
+    public Asiento findOne(Integer id);
+    public void delete(Integer id);
+    public List<Asiento> findAll();
 
 }
